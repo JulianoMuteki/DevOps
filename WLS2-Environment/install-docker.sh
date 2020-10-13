ac53af12920e19aaed750d9caa91464ee661fd59
Juliano Cesar Pestili <juliano.pestili@outlook.com>
2020-10-13 20:06:26 -0300

    Config environment

---
+++ WLS2-Environment/install-docker.sh
@@ -0,0 +1,16 @@
+ MK8S="mk8s"
+ 
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io
+sudo systemctl status docker
+sudo apt update -y  && sudo apt upgrade -y
+
+ sudo gpasswd -a $USER docker
+
+# DOCKER COMPOSE
+ sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+ sudo chmod +x /usr/local/bin/docker-compose
